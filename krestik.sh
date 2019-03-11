8be0e31a3791fa2fb34f0f9d4db5e30f00705018
Gor1194 <gorchshmarityan94@gmail.com>
2019-03-11 20:14:24 +0400

    Added game area and check func.

--- krestik.sh
+++ krestik.sh
@@ -1,1 +1,50 @@
+#!/bin/bash
+
+arr=("1" "2" "3" "4" "5" "6" "7" "8" "9")
+
+clear
+
+
+function game_area {
+    echo "Game area:"
+    echo "${arr[0]}  | ${arr[1]} | ${arr[2]}"
+    echo "___|___|___"
+    echo "${arr[3]}  | ${arr[4]} | ${arr[5]}"
+    echo "___|___|___"
+    echo "${arr[6]}  | ${arr[7]} | ${arr[8]}"
+    echo "   |   |"
+}
+
+
+
+function check {
+
+                if [[ ( ${arr[0]} == ${arr[1]} ) && ( ${arr[1]} == ${arr[2]} ) ]]; then
+                        echo "${arr[0]} won"; exit ; fi
+
+                if [[ ( ${arr[3]} == ${arr[4]} ) && ( ${arr[4]} == ${arr[5]} ) ]]; then
+                        echo "${arr[3]} won"; exit ; fi
+
+                if [[ ( ${arr[6]} == ${arr[7]} ) && ( ${arr[7]} == ${arr[8]} ) ]]; then
+                        echo "${arr[6]} won"; exit ; fi
+
+                if [[ ( ${arr[0]} == ${arr[3]} ) && ( ${arr[3]} == ${arr[6]} ) ]]; then
+                        echo "${arr[0]} won"; exit ; fi
+
+                if [[ ( ${arr[1]} == ${arr[4]} ) && ( ${arr[4]} == ${arr[7]} ) ]]; then
+                        echo "${arr[1]} won"; exit ; fi
+
+                if [[ ( ${arr[2]} == ${arr[5]} ) && ( ${arr[5]} == ${arr[8]} ) ]]; then
+                        echo "${arr[2]} won"; exit ; fi
+
+                if [[ ( ${arr[0]} == ${arr[4]} ) && ( ${arr[4]} == ${arr[8]} ) ]]; then
+                        echo "${arr[0]} won"; exit ; fi
+
+                if [[ ( ${arr[2]} == ${arr[4]} ) && ( ${arr[4]} == ${arr[6]} ) ]]; then
+                        echo "${arr[2]} won"; exit ; fi
+
+
+}
+
+game_area
 
