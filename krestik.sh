6ae0697647906cb52a267fdc39d603d314219a69
LevonPapyan <levonlevon31@gmail.com>
2019-03-12 00:07:50 +0400

    add input

--- krestik.sh
+++ krestik.sh
@@ -2,11 +2,8 @@
 
 arr=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 
-clear
-
-
 function game_area {
-    echo "Game area:"
+    clear
     echo "${arr[0]}  | ${arr[1]} | ${arr[2]}"
     echo "___|___|___"
     echo "${arr[3]}  | ${arr[4]} | ${arr[5]}"
@@ -15,7 +12,14 @@ function game_area {
     echo "   |   |"
 }
 
+function input {
+        read -n 1 index
+        while [[ ${arr[$index - 1]} == "X" || ${arr[$index - 1]} == "O" ]]
+        do
+                read -n 1 index
+        done
 
+}
 
 function check {
 
@@ -45,6 +49,21 @@ function check {
 
 
 }
+for (( i = 1; i <= 9; i++ )); 
+do
+        game_area 
+        check
+	input
+        if [ $((i%2)) -eq 1 ]
+        then
+                arr[$index - 1]="X"
+        else
+                arr[$index - 1]="O"
+        fi
+done
+echo "Nichya"
 
-game_area
+
+
+
 
