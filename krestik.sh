dcabf0d2cb0c3e8cb4a8afdb43212db7be1ee0e6
Arman199116 <arman.ochinyan@gmail.com>
2019-03-12 08:50:33 +0400

    task is done

--- krestik.sh
+++ krestik.sh
@@ -4,13 +4,8 @@ arr=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 
 clear
 
-green='\033[0;32m'
-red='\033[0;31m'
-nocolor='\033[0m'
-    
-
 function game_area {
-  echo "Game area:                Stepanavan/Training's GOOD job! "   
+  echo "Game area:              Stepanavan/Training's GOOD job! "   
   echo "7  | 8 | 9"
   echo "___|___|___"
   echo "4  | 5 | 6"
@@ -28,6 +23,7 @@ function game_area {
   echo -e "${arr[0]}  | ${arr[1]} | ${arr[2]}"
   echo -e "   |   |"
 }
+
 
 
 function check {
@@ -59,7 +55,7 @@ function check {
 
 }
 
-function check_reapet_index()
+function check_repeat_index()
 {
   while [[  ${arr[$index-1]} != $index  ]]
   do
@@ -70,23 +66,23 @@ function check_reapet_index()
   done
 }
 
-
+function game_X0(){
 game_area
 for (( i = 0; i < 9; i++ )); do
   if [[ $((i % 2)) == 0 ]]
   then
-       echo "First player's turn: (X) "
+       echo "First player's turn: (X)"
        read index
-       check_reapet_index
-       arr[$index-1]="${green}X${nocolor}"
+       check_repeat_index
+       arr[$index-1]='X'
        clear
        game_area
 
    else
-       echo "Second player's turn:(0) "
+       echo "Second player's turn: (0)"
        read index
-       check_reapet_index   #ստուգում է արդյոք վանդակի տվյալ տեղը ազատ է թե չէ
-       arr[$index-1]=${red}'0'${nocolor}
+       check_repeat_index   #ստուգում է արդյոք վանդակի տվյալ տեղը ազատ է թե չէ
+       arr[$index-1]='0'
        clear
        game_area
 
@@ -94,7 +90,11 @@ for (( i = 0; i < 9; i++ )); do
   check #ստուգում է թե ով է հաղթել
 
 done
-clear
+
 game_area
 
 echo "No Winner!"
+
+}
+
+game_X0
